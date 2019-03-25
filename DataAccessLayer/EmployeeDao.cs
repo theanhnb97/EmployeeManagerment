4ee76c64e407a766937d719de157ce0cde77552f
Thế Anh <33874999+theanhnb97@users.noreply.github.com>
2019-03-25 09:51:46 +0700

    Login

--- DataAccessLayer/EmployeeDao.cs
+++ DataAccessLayer/EmployeeDao.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,14 @@ using Entity;
 
 namespace DataAccessLayer
 {
-    public class EmployeeDao : IEntities<Employee>
+    interface IEmployee
     {
+        bool Login(string UserName, string Password);
+    }
+
+    public class EmployeeDao : IEntities<Employee>,IEmployee
+    {
+
         public List<Employee> Get()
         {
             throw new NotImplementedException();
@@ -32,6 +39,11 @@ namespace DataAccessLayer
         public int Add(Employee obj)
         {
             throw new NotImplementedException();
+        }
+
+        public bool Login(string UserName, string Password)
+        {
+            
         }
     }
 }
