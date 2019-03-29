e778b25e9b769cabca04214632e2e82552b3a254
leduchai <leduchaif@gmail.com>
2019-03-29 17:22:33 +0700

    .

---
+++ Main/FormMain_.designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Main
+{
+    partial class FormMain_
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain_));
+            this.pnTop = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnProfile = new System.Windows.Forms.PictureBox();
+            this.picLogout = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnMenu = new System.Windows.Forms.Panel();
+            this.btnEmployee = new System.Windows.Forms.Button();
+            this.btnPhanQuyen = new System.Windows.Forms.Button();
+            this.btnRole = new System.Windows.Forms.Button();
+            this.btnAction = new System.Windows.Forms.Button();
+            this.btnDepartment = new System.Windows.Forms.Button();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.pnSlide = new System.Windows.Forms.Panel();
+            this.btnSalary = new System.Windows.Forms.Button();
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bunifuElipseMain = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.pnTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnProfile)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogout)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pnMenu.SuspendLayout();
+            this.pnMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnTop
+            // 
+            this.pnTop.BackColor = System.Drawing.Color.LightCyan;
+            this.pnTop.Controls.Add(this.label2);
+            this.pnTop.Controls.Add(this.btnProfile);
+            this.pnTop.Controls.Add(this.picLogout);
+            this.pnTop.Controls.Add(this.label1);
+            this.pnTop.Controls.Add(this.pictureBox1);
+            this.pnTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnTop.Location = new System.Drawing.Point(0, 0);
+            this.pnTop.Name = "pnTop";
+            this.pnTop.Size = new System.Drawing.Size(1025, 50);
+            this.pnTop.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(241, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Test v1.0.1";
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.Image = ((System.Drawing.Image)(resources.GetObject("btnProfile.Image")));
+            this.btnProfile.Location = new System.Drawing.Point(934, 8);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.Size = new System.Drawing.Size(29, 28);
+            this.btnProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnProfile.TabIndex = 0;
+            this.btnProfile.TabStop = false;
+            this.btnProfile.Click += new System.EventHandler(this.btnProfile_Click);
+            // 
+            // picLogout
+            // 
+            this.picLogout.Image = ((System.Drawing.Image)(resources.GetObject("picLogout.Image")));
+            this.picLogout.Location = new System.Drawing.Point(979, 8);
+            this.picLogout.Name = "picLogout";
+            this.picLogout.Size = new System.Drawing.Size(29, 27);
+            this.picLogout.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogout.TabIndex = 0;
+            this.picLogout.TabStop = false;
+            this.picLogout.Click += new System.EventHandler(this.picLogout_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(236, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Management";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pnMenu
+            // 
+            this.pnMenu.BackColor = System.Drawing.Color.FloralWhite;
+            this.pnMenu.Controls.Add(this.btnEmployee);
+            this.pnMenu.Controls.Add(this.btnPhanQuyen);
+            this.pnMenu.Controls.Add(this.btnRole);
+            this.pnMenu.Controls.Add(this.btnAction);
+            this.pnMenu.Controls.Add(this.btnDepartment);
+            this.pnMenu.Controls.Add(this.btnTask);
+            this.pnMenu.Controls.Add(this.pnSlide);
+            this.pnMenu.Controls.Add(this.btnSalary);
+            this.pnMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnMenu.Location = new System.Drawing.Point(0, 50);
+            this.pnMenu.Name = "pnMenu";
+            this.pnMenu.Size = new System.Drawing.Size(216, 614);
+            this.pnMenu.TabIndex = 1;
+            // 
+            // btnEmployee
+            // 
+            this.btnEmployee.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnEmployee.FlatAppearance.BorderSize = 0;
+            this.btnEmployee.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEmployee.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmployee.Location = new System.Drawing.Point(17, 223);
+            this.btnEmployee.Name = "btnEmployee";
+            this.btnEmployee.Size = new System.Drawing.Size(199, 58);
+            this.btnEmployee.TabIndex = 7;
+            this.btnEmployee.Text = "           Nhân viên";
+            this.btnEmployee.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEmployee.UseVisualStyleBackColor = false;
+            this.btnEmployee.Click += new System.EventHandler(this.btnEmployee_Click);
+            // 
+            // btnPhanQuyen
+            // 
+            this.btnPhanQuyen.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnPhanQuyen.FlatAppearance.BorderSize = 0;
+            this.btnPhanQuyen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhanQuyen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPhanQuyen.Location = new System.Drawing.Point(17, 415);
+            this.btnPhanQuyen.Name = "btnPhanQuyen";
+            this.btnPhanQuyen.Size = new System.Drawing.Size(199, 58);
+            this.btnPhanQuyen.TabIndex = 6;
+            this.btnPhanQuyen.Text = "           Phân quyền";
+            this.btnPhanQuyen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPhanQuyen.UseVisualStyleBackColor = false;
+            this.btnPhanQuyen.Click += new System.EventHandler(this.btnPhanQuyen_Click);
+            // 
+            // btnRole
+            // 
+            this.btnRole.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnRole.FlatAppearance.BorderSize = 0;
+            this.btnRole.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRole.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRole.Location = new System.Drawing.Point(17, 351);
+            this.btnRole.Name = "btnRole";
+            this.btnRole.Size = new System.Drawing.Size(199, 58);
+            this.btnRole.TabIndex = 5;
+            this.btnRole.Text = "           Roles";
+            this.btnRole.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRole.UseVisualStyleBackColor = false;
+            this.btnRole.Click += new System.EventHandler(this.btnRole_Click);
+            // 
+            // btnAction
+            // 
+            this.btnAction.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnAction.FlatAppearance.BorderSize = 0;
+            this.btnAction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAction.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAction.Location = new System.Drawing.Point(17, 287);
+            this.btnAction.Name = "btnAction";
+            this.btnAction.Size = new System.Drawing.Size(199, 58);
+            this.btnAction.TabIndex = 4;
+            this.btnAction.Text = "           Hành động";
+            this.btnAction.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnAction.UseVisualStyleBackColor = false;
+            this.btnAction.Click += new System.EventHandler(this.btnAction_Click);
+            // 
+            // btnDepartment
+            // 
+            this.btnDepartment.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnDepartment.FlatAppearance.BorderSize = 0;
+            this.btnDepartment.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDepartment.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDepartment.Location = new System.Drawing.Point(17, 159);
+            this.btnDepartment.Name = "btnDepartment";
+            this.btnDepartment.Size = new System.Drawing.Size(199, 58);
+            this.btnDepartment.TabIndex = 3;
+            this.btnDepartment.Text = "           Phòng ban";
+            this.btnDepartment.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDepartment.UseVisualStyleBackColor = false;
+            this.btnDepartment.Click += new System.EventHandler(this.btnDepartment_Click);
+            // 
+            // btnTask
+            // 
+            this.btnTask.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnTask.FlatAppearance.BorderSize = 0;
+            this.btnTask.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTask.Location = new System.Drawing.Point(17, 95);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(199, 58);
+            this.btnTask.TabIndex = 2;
+            this.btnTask.Text = "           Công việc";
+            this.btnTask.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTask.UseVisualStyleBackColor = false;
+            this.btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // pnSlide
+            // 
+            this.pnSlide.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.pnSlide.Location = new System.Drawing.Point(1, 31);
+            this.pnSlide.Name = "pnSlide";
+            this.pnSlide.Size = new System.Drawing.Size(16, 58);
+            this.pnSlide.TabIndex = 1;
+            // 
+            // btnSalary
+            // 
+            this.btnSalary.BackColor = System.Drawing.Color.FloralWhite;
+            this.btnSalary.FlatAppearance.BorderSize = 0;
+            this.btnSalary.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalary.Location = new System.Drawing.Point(17, 31);
+            this.btnSalary.Name = "btnSalary";
+            this.btnSalary.Size = new System.Drawing.Size(199, 58);
+            this.btnSalary.TabIndex = 0;
+            this.btnSalary.Text = "           Lương";
+            this.btnSalary.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSalary.UseVisualStyleBackColor = false;
+            this.btnSalary.Click += new System.EventHandler(this.btnSalary_Click);
+            // 
+            // pnMain
+            // 
+            this.pnMain.Controls.Add(this.panel1);
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(216, 50);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(809, 614);
+            this.pnMain.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightCyan;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 588);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(809, 26);
+            this.panel1.TabIndex = 1;
+            // 
+            // bunifuElipseMain
+            // 
+            this.bunifuElipseMain.ElipseRadius = 15;
+            this.bunifuElipseMain.TargetControl = this;
+            // 
+            // FormMain_
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1025, 664);
+            this.Controls.Add(this.pnMain);
+            this.Controls.Add(this.pnMenu);
+            this.Controls.Add(this.pnTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(637, 417);
+            this.Name = "FormMain_";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Management";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.pnTop.ResumeLayout(false);
+            this.pnTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnProfile)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogout)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pnMenu.ResumeLayout(false);
+            this.pnMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnTop;
+        private System.Windows.Forms.Panel pnMenu;
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.PictureBox picLogout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnPhanQuyen;
+        private System.Windows.Forms.Button btnRole;
+        private System.Windows.Forms.Button btnAction;
+        private System.Windows.Forms.Button btnDepartment;
+        private System.Windows.Forms.Button btnTask;
+        private System.Windows.Forms.Panel pnSlide;
+        private System.Windows.Forms.Button btnSalary;
+        private System.Windows.Forms.Button btnEmployee;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox btnProfile;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipseMain;
+    }
+}
