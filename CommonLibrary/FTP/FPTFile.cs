d427e11157878d21a9b569eb04addac09958bc47
Thế Anh <33874999+theanhnb97@users.noreply.github.com>
2019-04-02 09:32:58 +0700

    okok

---
+++ CommonLibrary/FTP/FPTFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonLibrary
+{
+    public class FPTFile
+    {
+        public static string UploadFtpFile(string folderName, string fileName)
+        {
+            DataSet myDataSet=new DataSet();
+            myDataSet.ReadXml("FPTConfig.xml");
+            DataTable mySetting = myDataSet.Tables["setting"];
+            if (folderName == "")
+                folderName = mySetting.Rows[0]["defaultfoler"].ToString();
+            string absoluteFileName = Path.GetFileName(fileName);
+            string link = string.Format(@"http://{0}/{1}/{2}",mySetting.Rows[0]["domain"], folderName, absoluteFileName);
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format(@"ftp://{0}/{1}/{2}", mySetting.Rows[0]["host"], folderName, absoluteFileName)));
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.UsePassive = true;
+            request.UseBinary = true;
+            request.KeepAlive = false;
+            request.Credentials = new 
+                NetworkCredential(mySetting.Rows[0]["username"].ToString(), mySetting.Rows[0]["password"].ToString());
+            request.ConnectionGroupName = "group";
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            using (Stream fileStream = File.OpenRead(fileName))
+            using (Stream ftpStream = request.GetRequestStream())
+            {
+                byte[] buffer = new byte[10240];
+                int read;
+                while ((read = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ftpStream.Write(buffer, 0, read);
+                }
+            }
+            //MessageBox.Show("Upload success!");
+            Clipboard.SetText(link);
+            return link;
+        }
+    }
+}
--- Main/Task/AddTask.cs
+++ Main/Task/AddTask.cs
@@ -339,38 +339,12 @@ namespace Main
                 if (myDialog.ShowDialog() == DialogResult.OK)
                 {
                     //lblFile.Text = Path.GetFileName(myDialog.FileName);
-                    linkFile.Text= UploadFtpFile("",myDialog.FileName);
+                    linkFile.Text=CommonLibrary.FPTFile.UploadFtpFile("",myDialog.FileName);
                 }
             }
         }
 
-        public string UploadFtpFile(string folderName, string fileName)
-        {
-            folderName = "hinhanh";
-            string absoluteFileName = Path.GetFileName(fileName);
-            string link = string.Format(@"http://{0}/{1}/{2}", "theanhnb97.000webhostapp.com", folderName, absoluteFileName);
-            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri(string.Format(@"ftp://{0}/{1}/{2}", "files.000webhost.com/public_html", folderName, absoluteFileName)));
-            request.Method = WebRequestMethods.Ftp.UploadFile;
-            request.UsePassive = true;
-            request.UseBinary = true;
-            request.KeepAlive = false;
-            request.Credentials = new NetworkCredential("theanhnb97", "nb791111");
-            request.ConnectionGroupName = "group";
-            request.Method = WebRequestMethods.Ftp.UploadFile;
-            using (Stream fileStream = File.OpenRead(fileName))
-            using (Stream ftpStream = request.GetRequestStream())
-            {
-                byte[] buffer = new byte[10240];
-                int read;
-                while ((read = fileStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    ftpStream.Write(buffer, 0, read);
-                }
-            }
-            MessageBox.Show("Upload success!");
-            Clipboard.SetText(link);
-            return link;
-        }
+        
 
         private void linkFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
